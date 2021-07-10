70cad0d026d9676185236796b3ee65e4f6194e55
vicky-ogutu <vikogutu77@gmail.com>
2021-07-11 01:55:21 +0300

    Initial commit

--- app/src/main/java/com/example/kotlinretrofit/RecyAdapter.kt
+++ app/src/main/java/com/example/kotlinretrofit/RecyAdapter.kt
@@ -1,27 +1,45 @@
 package com.example.kotlinretrofit
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyAdapter: RecyclerView.Adapter<RecyAdapter.myViewHolder>() {
 
+ var posts_list = mutableListOf<Posts>()
+
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyAdapter.myViewHolder{
-        TODO("Not yet implemented")
+
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.postlist_layout, parent, false);
+
+        return myViewHolder(inflater);
     }
 
     override fun onBindViewHolder(holder: RecyAdapter.myViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(posts_list[position])
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return posts_list.size
     }
 
     class myViewHolder (view: View) : RecyclerView.ViewHolder(view){
 
+        val textTitle = view.findViewById<TextView>(R.id.body);
+        val textBody = view.findViewById<TextView>(R.id.title);
+
+
+        fun bind(data: Posts){
+
+            textTitle.text = data.title
+            textBody.text = data.body
+
+        }
 
     }
 
